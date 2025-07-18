dee34566d2bad8de7d8f934407a097c8109d56fd
Amelia Trevino <trevinoamelia25@gmail.com>
2025-07-18 12:06:58 -0500

    Created Agent Cards on Home Page

--- components/AgentCard.tsx
+++ components/AgentCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface AgentCardProps{
     id: string;
@@ -8,7 +9,7 @@ interface AgentCardProps{
     duration: number;
     color: string;
 }
-//
+
 const AgentCard = ({ id, name, topic, category, duration, color }: AgentCardProps) => {
   return (
     <article className="agent-card" style={{ backgroundColor: color }}>
@@ -18,7 +19,16 @@ const AgentCard = ({ id, name, topic, category, duration, color }: AgentCardProp
                 <Image src="/icons/bookmark.svg" alt="Bookmark" width={12.5} height={15} />
             </button>
         </div>
-      
+        <h2 className="text-2xl font-bold">{name}</h2>
+        <p className="text-sm">{topic}</p>
+        <div className="flex items-center gap-2">
+            <Image src="/icons/clock.svg" alt="Clock" width={16} height={16} />
+            <p className="text-sm">{duration} minutes</p>
+        </div>
+        <Link href={`/agents/${id}`} className="w-full">
+            <button className="btn-primary w-full justify-center">Open Lesson</button>
+        </Link>
+        
     </article>
     );
 }
